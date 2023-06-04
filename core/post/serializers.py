97990726c9f8bc7f2c724ac712b2216ff764fac4
Timur Tuleuov <timmmur123@gmail.com>
2023-06-04 22:00:51 +0600

    Fix post serializer

--- core/post/serializers.py
+++ core/post/serializers.py
@@ -47,5 +47,5 @@ class PostSerializer(AbstractSerializer):
     class Meta:
         model = Post
         # List of all the fields that can be included in a request or a response
-        fields = ['id', 'author', 'body', 'edited', 'liked', 'likes_count', 'created', 'updated']
+        fields = ['id', 'author', 'body', 'cover', 'edited', 'liked', 'likes_count', 'created', 'updated']
         read_only_fields = ["edited"]
