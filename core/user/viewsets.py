ca4baf50b401cb8db05184a775a8d7824bdcaf9d
Timur Tuleuov <timmmur123@gmail.com>
2023-06-20 21:52:51 +0600

    Fix user viewset

--- core/user/viewsets.py
+++ core/user/viewsets.py
@@ -3,8 +3,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 
 from core.abstract.viewsets import AbstractViewSet
-from core.post.models import Post
-from core.post.serializers import PostSerializer
+from core.comment.models import Comment
+from core.comment.serializers import CommentSerializer
+from core.post.models import Post, PostImageMapping
+from core.post.serializers import PostImageMappingSerializer, PostSerializer
 from core.user.serializers import UserSerializer
 from core.user.models import User
 
@@ -24,9 +26,25 @@ class UserViewSet(AbstractViewSet):
         self.check_object_permissions(self.request, obj)
         return obj
 
+    
     @action(detail=True, methods=['get'], permission_classes=[]) 
     def get_user_posts(self, request, *args, **kwargs):
         user = User.objects.get_object_by_public_id(self.kwargs['pk'])
         posts = Post.objects.filter(author=user)
         serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+        data = serializer.data
+
+        # Добавить связанные изображения и комментарии в каждый объект поста
+        for post_data in data:
+            post_id = post_data['id']
+            # Получить изображения для текущего поста
+            post_covers = PostImageMapping.objects.filter(post_uuid=post_id)
+            cover_serializer = PostImageMappingSerializer(post_covers, many=True)
+            post_data['cover'] = cover_serializer.data
+
+            # Получить комментарии для текущего поста
+            comments = Comment.objects.filter(post_uuid=post_id)
+            comment_serializer = CommentSerializer(comments, many=True)
+            post_data['comment'] = comment_serializer.data
+
+        return Response(data)
